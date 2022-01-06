de9839caf2f5b72b5584abc3e59ebab60080c8ea
denisjovic <redzistrejsn@pm.me>
2022-01-05 20:31:07 -0500

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import { Route} from "react-router-dom";
+
+function HatsPage () {
+    return <h1>Hats page</h1>
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
     </div>
   );
 }
