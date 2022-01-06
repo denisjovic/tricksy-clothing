e3bf541d246eed5122022b090fee9f154c9d77d9
denisjovic <redzistrejsn@pm.me>
2022-01-06 17:36:49 -0500

    added shop page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
-import { Route} from "react-router-dom";
-
-function HatsPage () {
-    return <h1>Hats page</h1>
-}
+import ShopPage from "./pages/shop/shop.component";
+import { Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route path='/shop' component={ShopPage}/>
+        </Switch>
     </div>
   );
 }
